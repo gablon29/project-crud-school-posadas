331034a03fa4bae273b7fbb051710a47abbe2bd4
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-10 18:17:53 -0300

    Creando la conexion a la base de datos/learning

--- src/Alumnos.java
+++ src/Alumnos.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.net.SocketOption;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Alumnos {
     private JPanel panel;
@@ -11,4 +15,13 @@ public class Alumnos {
     private JButton crearButton;
     private JButton consultarButton;
     private JList list1;
+    Connection con;
+    public void conectar() {
+        try {
+            con= DriverManager.getConnection("localhost/learning", "gabriel", "291194Cg@ñ");
+            System.out.println("Conectado");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
