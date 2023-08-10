56a712e1a6d4b7368ed1d4da4bac037b973c5ee2
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-10 17:24:28 -0300

    Create a projecto-crud-demo.iml

---
+++ src/Alumnos.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Alumnos {
+    private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton crearButton;
+    private JButton consultarButton;
+    private JList list1;
+}
