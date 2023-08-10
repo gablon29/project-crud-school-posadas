e15102f2e1a0f3f9611185eceb11568f5003ef1e
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-10 19:14:04 -0300

    testing the database conecct succesfully

--- src/Alumnos.java
+++ src/Alumnos.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.SocketOption;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Alumnos {
+public class Alumnos extends JFrame {
     private JPanel panel;
     private JTextField textField1;
     private JTextField textField2;
@@ -16,9 +18,26 @@ public class Alumnos {
     private JButton consultarButton;
     private JList list1;
     Connection con;
+    public Alumnos() {
+        consultarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conectar();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Alumnos f = new Alumnos();
+        f.setContentPane(new Alumnos().panel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.pack();
+    }
+
     public void conectar() {
         try {
-            con= DriverManager.getConnection("localhost/learning", "gabriel", "291194Cg@ñ");
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/learning", "gabriel", "291194Cg@ñ");
             System.out.println("Conectado");
         } catch (SQLException e) {
             throw new RuntimeException(e);
